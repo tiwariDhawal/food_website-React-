c2be9331f22cad73fb7cf2bd75d53d08f91686f5
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-07-15 17:20:11 +0530

    making items removeable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,9 +8,13 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItems = (
